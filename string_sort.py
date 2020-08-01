b2feeafc35273e048ca6bca4a4b3ac3ebe5f42d0
Kerem Balci <kerembalci90@gmail.com>
2020-08-01 01:22:14 -0400

    Challange 3: Join words at the end

--- string_sort.py
+++ string_sort.py
@@ -4,5 +4,4 @@
 def sort_word_list(full_string):
     list_of_words = full_string.split()
     list_of_words.sort(key=str.lower)
-    # sorted_list_of_words = sorted(list_of_words, key=str.lower)
-    print(list_of_words)
+    return ' '.join(list_of_words)
